22252969ddbf8836a73947b2adbb055a6fcb587b
SharafEldein <moahmedsharaf@aucegypt.edu>
2023-01-21 10:14:37 +0200

    Socket() syscall

--- asm_server.asm
+++ asm_server.asm
@@ -3,9 +3,15 @@
 
 .section .text
 _start:
-	
+
+	mov rdi, 2 
+	mov rsi, 1
+	mov rdx, 0
+	mov rax, 0x29 	# sys_socket socket(AF_INET, SOCK_STREAM, IPProto_IP)
+	syscall
+
 	mov rdi, 0
-	mov rax, 0x3C #sys_Exit
+	mov rax, 0x3C 	# sys_exit exit(0)
 	syscall
 
 .section .data
