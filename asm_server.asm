f0e60729d7637530bd53523b1e5a9afef7747f2f
SharafEldein <moahmedsharaf@aucegypt.edu>
2023-01-21 11:39:36 +0200

    Accepting connection

--- asm_server.asm
+++ asm_server.asm
@@ -7,11 +7,33 @@ _start:
 	mov rdi, 2 
 	mov rsi, 1
 	mov rdx, 0
-	mov rax, 0x29 	# sys_socket socket(AF_INET, SOCK_STREAM, IPProto_IP)
+	mov rax, 0x29 	# socket(AF_INET, SOCK_STREAM, IPProto_IP)
 	syscall
 
+	mov rdi, 3
+	lea rsi, [rip+structaddr]
+	mov rdx, 16
+	mov rax, 0x31 	# bind(3, {sa_family=AF_INET, sin_port=htons(80), sin_addr=inet_addr("0.0.0.0"), 0}, 16)
+ 	syscall 
+
+	mov rdi, 3
+	mov rsi, 0
+	mov rax, 0x32	# listen(3,0)
+	syscall
+
+	mov rdi, 3
+	mov rsi, 0
+	mov rdx, 0
+	mov rax, 2B 	# accept(3, NULL, NULL)
+	syscall
+	
 	mov rdi, 0
-	mov rax, 0x3C 	# sys_exit exit(0)
+	mov rax, 0x3C 	# exit(0)
 	syscall
 
 .section .data
+structaddr:
+	.2byte 2
+	.2byte 0x5000
+	.4byte 0
+	.byte  0
