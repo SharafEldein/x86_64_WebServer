9ca2f064f3af4607153816b7e6b874db6d0c6843
SharafEldein <moahmedsharaf@aucegypt.edu>
2023-01-22 02:08:44 +0200

    Forking on connect and http get request [fork syscall]

--- asm_server.asm
+++ asm_server.asm
@@ -4,103 +4,122 @@
 .section .text
 _start:
 
-	mov rdi, 2
-	mov rsi, 1
-	mov rdx, 0
-	mov rax, 0x29 	# socket(AF_INET, SOCK_STREAM, IPProto_IP)
-	syscall
+        mov rdi, 2
+        mov rsi, 1
+        mov rdx, 0
+        mov rax, 0x29   # socket(AF_INET, SOCK_STREAM, IPProto_IP)
+        syscall
 
-	mov rdi, 3
-	lea rsi, [rip+structaddr]
-	mov rdx, 16
-	mov rax, 0x31 	# bind(3, {sa_family=AF_INET, sin_port=htons(80), sin_addr=inet_addr("0.0.0.0"), 0}, 16)
- 	syscall
+        mov rdi, 3
+        lea rsi, [rip+structaddr]
+        mov rdx, 16
+        mov rax, 0x31   # bind(3, {sa_family=AF_INET, sin_port=htons(80), sin_addr=inet_addr("0.0.0.0"), 0}, 16)
+        syscall
 
-	mov rdi, 3
-	mov rsi, 0
-	mov rax, 0x32	# listen(3,0)
-	syscall
+        mov rdi, 3
+        mov rsi, 0
+        mov rax, 0x32   # listen(3,0)
+        syscall
 
-	mov rdi, 3
-	mov rsi, 0
-	mov rdx, 0
-	mov rax, 0x2B 	# accept(3, NULL, NULL)
-	syscall
+        mov rdi, 3
+        mov rsi, 0
+        mov rdx, 0
+        mov rax, 0x2B   # accept(3, NULL, NULL)
+        syscall
 
-	mov rdi, 4
-	lea rsi, rsp	# read starting from stack pointer till nullByte
-	mov rdx, 512
-	mov rax, 0x0	# read(4, req, req.size)
-	syscall
+        mov rax, 0x39
+        syscall
+        
+        cmp rax, 0
+        je child
 
+        mov rdi, 4
+        mov rax, 0x3    # close(4)
+        syscall
 
-	loop_first_space:
-		mov r10b, [rsi]
-		cmp r10b, ' '
-		je done
-		inc rsi
-		jmp loop_first_space
-
-	done:
-		inc rsi
-
-	mov r11, rsi	# saving start of the path
-	loop_second_space:
-		mov r10b, [rsi]
-		cmp r10b, ' '
-		je done_2
-		inc rsi
-		jmp loop_second_space
-	done_2:
-		mov byte ptr [rsi], 0	# nullByteing the end of string
-
-
-	mov rdi, r11
-	mov rsi, 0
-	mov rdx, 0
-	mov rax, 0x2	# open("path", flags, mode)
-	syscall
+        mov rdi, 3
+        mov rsi, 0
+        mov rdx, 0
+        mov rax, 0x2B   # accept(3, NULL, NULL)
+        syscall
+        
+        jmp all_done
+child:
+        mov rdi, 3
+        mov rax, 0x3    # close(3)
+        syscall
+        
+        mov rdi, 4
+        lea rsi, [rsp]  # read starting from stack pointer till nullByte
+        mov rdx, 512
+        mov rax, 0x0    # read(4, req, req.size)
+        syscall
 
 
-	mov rdi, 5
-	lea rsi, [rsp]
-	mov rdx, 512
-	mov rax, 0x0	# read(5, file_content, file_content.size)
-	syscall
-	push rax 		# getting size of content to write
+        loop_first_space:
+                mov r10b, [rsi]
+                cmp r10b, ' '
+                je done
+                inc rsi
+                jmp loop_first_space
 
-	mov rdi, 5
-	mov rax, 0x3	# close(5)
-	syscall
+        done:
+                inc rsi
 
-	mov rdi, 4
-	lea rsi, [rip+str]
-	mov rdx, 19
-	mov rax, 0x1	# write(4, 200 Ok, response.size)
-	syscall
-
-	pop r11
-	mov rdi, 4
-	lea rsi, [rsp]
-	mov rdx, r11
-	mov rax, 1		# write(4, response, response.size)
-	syscall
+        mov r11, rsi    # saving start of the path
+        loop_second_space:
+                mov r10b, [rsi]
+                cmp r10b, ' '
+                je done_2
+                inc rsi
+                jmp loop_second_space
+        done_2:
+                mov byte ptr [rsi], 0   # nullByteing the end of string
 
 
-	mov rdi, 4
-	mov rax, 0x3	# close(4)
-	syscall
+        mov rdi, r11
+        mov rsi, 0
+        mov rdx, 0
+        mov rax, 0x2    # open("path", flags, mode)
+        syscall
 
-	mov rdi, 0
-	mov rax, 0x3C 	# exit(0)
-	syscall
 
+        mov rdi, 3
+        lea rsi, [rsp]
+        mov rdx, 512
+        mov rax, 0x0    # read(5, file_content, file_content.size)
+        syscall
+        push rax        # getting size of content to write
+
+        mov rdi, 3
+        mov rax, 0x3    # close(3)
+        syscall
+
+        mov rdi, 4
+        lea rsi, [rip+str]
+        mov rdx, 19
+        mov rax, 0x1    # write(4, 200 Ok, response.size)
+        syscall
+
+        pop r11
+        mov rdi, 4
+        lea rsi, [rsp]
+        mov rdx, r11
+        mov rax, 1      # write(4, response, response.size)
+        syscall
+
+
+all_done:
+        mov rdi, 0
+        mov rax, 0x3C
+        syscall
+        
 .section .data
 structaddr:
-	.2byte 2
-	.2byte 0x5000
-	.4byte 0
-	.byte  0
+        .2byte 2
+        .2byte 0x5000
+        .4byte 0
+        .byte  0
 
 str:
-	.string "HTTP/1.0 200 OK\r\n\r\n"
+        .string "HTTP/1.0 200 OK\r\n\r\n"
